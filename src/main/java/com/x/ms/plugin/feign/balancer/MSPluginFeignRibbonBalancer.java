41075f13ea3232dade81b89e981249697c43d934
xiongyihua <xiongyihua@hikcreate.com>
2021-02-25 16:24:25 +0800

    update balancer

--- src/main/java/com/x/ms/plugin/feign/balancer/MSPluginFeignRibbonBalancer.java
+++ src/main/java/com/x/ms/plugin/feign/balancer/MSPluginFeignRibbonBalancer.java
@@ -1,19 +1,29 @@
 package com.x.ms.plugin.feign.balancer;
 
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.loadbalancer.Request;
 import org.springframework.cloud.client.loadbalancer.Response;
-import org.springframework.cloud.loadbalancer.core.ReactorServiceInstanceLoadBalancer;
+import org.springframework.cloud.loadbalancer.core.RoundRobinLoadBalancer;
+import org.springframework.cloud.loadbalancer.core.ServiceInstanceListSupplier;
 import reactor.core.publisher.Mono;
 
 /**
  * 根据灰度配置完成灰度插件负载均衡
  */
-public class MSPluginFeignRibbonBalancer implements ReactorServiceInstanceLoadBalancer {
+public class MSPluginFeignRibbonBalancer extends RoundRobinLoadBalancer {
+
+    public MSPluginFeignRibbonBalancer(ObjectProvider<ServiceInstanceListSupplier> serviceInstanceListSupplierProvider, String serviceId) {
+        super(serviceInstanceListSupplierProvider, serviceId);
+    }
+
+    public MSPluginFeignRibbonBalancer(ObjectProvider<ServiceInstanceListSupplier> serviceInstanceListSupplierProvider, String serviceId, int seedPosition) {
+        super(serviceInstanceListSupplierProvider, serviceId, seedPosition);
+    }
 
     @Override
     public Mono<Response<ServiceInstance>> choose(Request request) {
-        //TODO
-        return null;
+        // TODO
+        return super.choose(request);
     }
 }
